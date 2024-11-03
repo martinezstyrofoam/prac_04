2677963475324a983a49ec1b67c26002af2e8de5
Siron Tommy Aban <siron.aban@my.jcu.edu.au>
2024-11-03 21:31:35 +1000

    subject_reader.py was completed. I was a stumped on what I had to do but realised it just wanted me to append to a list, then print every row of said list.

--- subject_reader.py
+++ subject_reader.py
@@ -7,23 +7,26 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subjects = load_data() # Data is returned from func load_data(). In this case, it would be load_data(subject)
+    # print(subjects)
+    print_subject(subjects)
+
+
+def print_subject(subjects):
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students!")
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject.append(parts) # Subject gets data taken and stored in parts, and added into a list.
     input_file.close()
+    return subject # Because this is in a function, return is needed to return data.
 
 
 main()
